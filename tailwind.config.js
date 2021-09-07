464221d4d4bc00e19ed305fa7ce8e10c1e64f6a2
Rishabh Malik <workwithmalik97@gmail.com>
2021-09-07 23:32:15 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      inset: {
+        '-13': '-3.25rem',
+        '-15': '-3.75rem',
+        '-17': '-4.25rem',
+        '-18': '-4.5rem',
+      },
+      spacing: {
+        70: '17rem',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
